f639687cd3df40ffd6e0690de3392a4ca0d4eefe
Karabin <maxkarab@yandex.ru>
2024-06-01 17:38:44 +0200

    UniTask

--- Assets/Runtime/ResourcesDescriptorSourceProvider.cs
+++ Assets/Runtime/ResourcesDescriptorSourceProvider.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using com.karabaev.descriptors.abstractions.Initialization;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
@@ -9,7 +8,7 @@ namespace com.karabaev.descriptors.unity
 {
   public class ResourcesDescriptorSourceProvider : IDescriptorSourceProvider
   {
-    public async ValueTask<IDescriptorRegistrySource> GetAsync(string key, Type type)
+    public async UniTask<IDescriptorRegistrySource> GetAsync(string key, Type type)
     {
       var resource = await Resources.LoadAsync(key, type).ToUniTask();
 
